94c4b3a9415c832dd2f36fa6aabd26e918ed0f81
Mykael Adam <mykael.adam@radio-canada.ca>
2026-04-20 11:05:25 -0400

    WIP

--- client/src/lib/auth-client.ts
+++ client/src/lib/auth-client.ts
@@ -7,6 +7,7 @@ import { adminClient } from "better-auth/client/plugins";
 export const authClient = createAuthClient({
   /** The base URL of the server (optional if you're using the same domain) */
   baseURL: "http://localhost:9000",
+  basePath: "/api/auth", // This tells the client where auth endpoints are mounted
   plugins: [inferAdditionalFields<typeof auth>(), adminClient()],
 });
 export const { signIn, signOut, signUp, updateUser, admin } = authClient;
